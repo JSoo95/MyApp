64df4821c35b9d3960f6822f0bb8baec3373f036
Jsoo95 <sinsuyang95@naver.com>
2017-10-17 18:50:21 +0900

    string
    string확인

--- app/src/main/java/com/hansung/android/myapp/RestaurantDetail.java
+++ app/src/main/java/com/hansung/android/myapp/RestaurantDetail.java
@@ -1,7 +1,10 @@
 package com.hansung.android.myapp;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
 
 public class RestaurantDetail extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class RestaurantDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
+
+        ArrayList<MyItem> data = new ArrayList<MyItem>();
+        data.add(new MyItem(R.drawable.ganjang, "간장", "3000"));
+
+
+        MyAdapter adapter = new MyAdapter(this, R.layout.item, data);
+
+
+        ListView list = (ListView) findViewById(R.id.listView);
+        list.setAdapter(adapt);
+
+
     }
 }
